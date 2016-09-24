426198eb7e0c1f8a180dbe291b349a0356a8e94f
Anders Damsgaard Christensen <adc@geo.au.dk>
2016-09-23 23:10:05 -0700

    create final combined plot

--- siple/runme.m
+++ siple/runme.m
@@ -1,7 +1,7 @@
 %% general script settings
 plot_data_sets = false;
 plot_meshes = true;
-plot_geometry = false;
+plot_geometry = true;
 plot_grounding_line = true;
 plot_friction_coefficient = true;
 
@@ -41,7 +41,7 @@ if plot_data_sets
 end
 
 
-%% Define model limits 
+%% Define model limits
 % 1. Press 'add a contour (closed)'
 % 2. Click around area of interest (no need to close polygon)
 % 3. Press <Enter>
@@ -69,43 +69,27 @@ err = 8;         % maximum error between interpolated and control field
 md = bamg(model, 'domain', domain, 'hmax', hinit);
 %plotmodel(md,'data','mesh')
 
+% TODO: check if the velocity data below is oriented correctly
 % interpolate velocities onto coarse mesh
-vx_obs = InterpFromGridToMesh(...
+vx_obs = InterpFromGridToMesh( ...
     rignot.x, rignot.y, ...
     rignot.vx, ...
     md.mesh.x, md.mesh.y, ...
     0);
 
-vy_obs = InterpFromGridToMesh(...
+vy_obs = InterpFromGridToMesh( ...
     rignot.x, rignot.y, ...
     rignot.vy, ...
     md.mesh.x, md.mesh.y, ...
     0);
- 
+
 vel_obs = sqrt(vx_obs.^2 + vy_obs.^2);
-
-if plot_geometry
-    figure
-    
-    imagesc(md.mesh.x, md.mesh.y, log10(vel_obs + 1));
-    axis xy equal tight
-    
-    title('Observed velocity')
-    xlabel('x [m]')
-    ylabel('y [m]')
-    
-    cb = colorbar();
-    ylabel(cb,'log_1_0(v) [m/yr]')
-
-    saveas(gcf, 'figures/siple_velocity_obs')
-    saveas(gcf, 'figures/siple_velocity_obs.pdf')
-end
 
 % adapt the mesh to minimize error in velocity interpolation
 md = bamg(md, 'hmax', hmax, 'hmin', hmin, ...
     'gradation', gradation, 'field', vel_obs, 'err', err);
 
-clear vx_obs vy_obs vel_obs;
+%clear vx_obs vy_obs vel_obs;
 
 if plot_meshes
     plotmodel(md, 'data', 'mesh')
@@ -128,7 +112,7 @@ groundedice = double(InterpFromGridToMesh(...
 
 % fill in the md.mask structure
 % ice is grounded for mask equal one
-md.mask.groundedice_levelset = groundedice; 
+md.mask.groundedice_levelset = groundedice;
 clear groundedice
 
 % interpolate onto our mesh vertices
@@ -144,8 +128,10 @@ clear ice
 
 if plot_grounding_line
     plotmodel(md, ...
-        'data', md.mask.groundedice_levelset, 'title', 'grounded/floating', ...
-        'data', md.mask.ice_levelset, 'title', 'ice/no-ice');
+        'data', md.mask.groundedice_levelset, ...
+        'title', 'grounded/floating', ...
+        'data', md.mask.ice_levelset, ...
+        'title', 'ice/no-ice');
     saveas(gcf, 'figures/siple_grounding_line')
     saveas(gcf, 'figures/siple_grounding_line.pdf')
 end
@@ -205,15 +191,33 @@ md.friction.coefficient = ...
     md.results.StressbalanceSolution.FrictionCoefficient;
 
 if plot_friction_coefficient
-    plotmodel(md,'data', md.friction.coefficient)
+    plotmodel(md, 'data', md.friction.coefficient)
     saveas(gcf, 'figures/siple_friction')
     saveas(gcf, 'figures/siple_friction.pdf')
 end
-    
+
 % Save model
 save models/siple_control_drag md;
 
-
+%% Post visualization
+plotmodel(md, 'nlines', 2, 'ncols', 2, ...
+    'unit#all', 'km', 'axis#all', 'equal', ...
+    'xlim#all', [min(md.mesh.x) max(md.mesh.x)] / 10^3, ...
+    'ylim#all', [min(md.mesh.y) max(md.mesh.y)] / 10^3, ...
+    'FontSize#all', 12, ...
+    'data', md.initialization.vel, ...
+    'title', 'Observed velocity', ...
+    'data', md.results.StressbalanceSolution.Vel, ...
+    'title', 'Modeled Velocity', ...
+    'data', md.geometry.base, ...
+    'title', 'Bed elevation', ...
+    'data', md.results.StressbalanceSolution.FrictionCoefficient, ...
+    'title', 'Friction Coefficient', ...
+    'colorbar#all', 'on', 'colorbartitle#1-2', '[m/yr]', ...
+    'caxis#1-2', ([1.5,4000]), ...
+    'colorbartitle#3', '[m]', 'log#1-2', 10);
+saveas(gcf, 'figures/siple_combined')
+saveas(gcf, 'figures/siple_combined.pdf')
 
 
 
